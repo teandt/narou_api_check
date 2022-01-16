73cebf59f478f2c6b3cfd2efaa78fa6411b79c98
tea <teandt@gmail.com>
2022-01-16 21:39:57 +0900

    カウンタのチェックを追加

--- narou_api.py
+++ narou_api.py
@@ -13,23 +13,61 @@ cont = gzip.decompress(res.content).decode("utf-8")
 
 res_json = json.loads(cont)
 
-db_connect = pymysql.connect(host='127.0.0.1',
-                            port=23306,
-                            user='narouDB',
-                            password='narouDB',
-                            database='narou_db',
-                            charset='utf8mb4',
-                            cursorclass=pymysql.cursors.DictCursor)
+def db_connect():
+    db = pymysql.connect(host='127.0.0.1',
+                        port=23306,
+                        user='narouDB',
+                        password='narouDB',
+                        database='narou_db',
+                        charset='utf8mb4',
+                        cursorclass=pymysql.cursors.DictCursor)
+    return db
+
+def check_count():
+    db = db_connect()
+
+    try:
+        with db.cursor() as cursor:
+            sql = "SELECT parameter_value  FROM parameter_tbl WHERE parameter_name='counter'"
+            cursor.execute(sql)
+            result = cursor.fetchone()            
+    except:
+        db.close()
+    else:
+        db.close()
+    
+    return result["parameter_value"]
+
+
+cnt = check_count()
+
+# try:
+#     db = db_connect()
+#     with db.cursor() as cursor:
+#         for i in res_json:
+#             if("ncode" in i):
+#                 sql = "INSERT INTO contents_tbl SET count = %s, ncode = %s, title = %s"
+#                 cursor.execute(sql, (cnt + 1, i["ncode"], i["title"]))
+        
+#         sql = "UPDATE parameter_tbl SET parameter_value = %s WHERE parameter_name = 'counter'"
+#         cursor.execute(sql, (cnt + 1))
+# except:
+#     db.rollback()
+#     db.close()
+# else:
+#     db.commit()
+#     db.close()
+
 
 try:
-    with db_connect.cursor() as cursor:
-        for i in res_json:
-            if("ncode" in i):
-                sql = "INSERT INTO contents_tbl SET count = 1, ncode = %s, title = %s"
-                cursor.execute(sql, (i["ncode"], i["title"]))
+    db = db_connect()
+    with db.cursor() as cursor:
+        sql = "SELECT title FROM contents_tbl WHERE count=%s"
+        cursor.execute(sql,(cnt))
+        result = cursor.fetchall()
+        for i in result:
+            print(i["title"])
 except:
-    db_connect.rollback()
-    db_connect.close()
-finally:
-    db_connect.commit()
-    db_connect.close()
+    db.close()
+else:
+    db.close()
