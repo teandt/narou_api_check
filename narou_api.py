8fe0259a0f67e4487ac99e55810345ed63527ca4
tea <teandt@gmail.com>
2022-01-16 20:41:54 +0900

    mysqlへSQL飛ばしてコミットできるところまで

--- narou_api.py
+++ narou_api.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import gzip
+import pymysql.cursors
 
 
 url = "http://api.syosetu.com/novelapi/api/"
@@ -12,11 +13,23 @@ cont = gzip.decompress(res.content).decode("utf-8")
 
 res_json = json.loads(cont)
 
-for i in res_json:
-    print(i)
+db_connect = pymysql.connect(host='127.0.0.1',
+                            port=23306,
+                            user='narouDB',
+                            password='narouDB',
+                            database='narou_db',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
 
-
-# for i in res_json:
-#     if("title" in i):
-#         print(i)
-
+try:
+    with db_connect.cursor() as cursor:
+        for i in res_json:
+            if("ncode" in i):
+                sql = "INSERT INTO contents_tbl SET count = 1, ncode = %s, title = %s"
+                cursor.execute(sql, (i["ncode"], i["title"]))
+except:
+    db_connect.rollback()
+    db_connect.close()
+finally:
+    db_connect.commit()
+    db_connect.close()
