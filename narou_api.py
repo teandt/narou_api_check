638eb1be5fd5c9fa046e9c6827262a0e88e7bbde
tea <teandt@gmail.com>
2022-01-17 20:01:12 +0900

    設定済みのDBに対応するデータを取得するように修正

--- narou_api.py
+++ narou_api.py
@@ -31,25 +31,36 @@ def check_count():
         with db.cursor() as cursor:
             sql = "SELECT parameter_value  FROM parameter_tbl WHERE parameter_name='counter'"
             cursor.execute(sql)
-            result = cursor.fetchone()            
-    except:
-        db.close()
-    else:
+            data = cursor.fetchone()
+            if data == None:
+                print("counter value data is none")
+                return -1
+            else:
+                result = data
+    finally:
         db.close()
     
     return result["parameter_value"]
 
 
 cnt = check_count()
-timestamp = datetime.datetime.now().isoformat(timespec='seconds')
+if cnt > 0:
+    timestamp = datetime.datetime.now().isoformat(timespec='seconds')
+else:
+    print("error cnt value")
+    exit()
 
 try:
     db = db_connect()
     with db.cursor() as cursor:
         for i in res_json:
             if("ncode" in i):
-                sql = "INSERT INTO contents_tbl SET count = %s, ncode = %s, title = %s"
-                cursor.execute(sql, (cnt + 1, i["ncode"], i["title"]))
+                sql = "INSERT INTO contents_tbl SET count = %s, ncode = %s, title = %s, userid = %s, writer = %s, story = %s, biggenre = %s, genre = %s, \
+                      gensaku = %s, keyword = %s, general_firstup = %s, general_lastup = %s, novel_type = %s, end = %s, general_all_no = %s, \
+                      length = %s, time = %s, isstop = %s, isr15 = %s"
+                cursor.execute(sql, (cnt + 1, i["ncode"], i["title"], i["userid"], i["writer"], i["story"], i["biggenre"], i["genre"],\
+                                     i["gensaku"], i["keyword"], i["general_firstup"], i["general_lastup"], i["novel_type"], i["end"], i["general_all_no"], \
+                                     i["length"], i["time"], i["isstop"], i["isr15"]))
         
         sql = "UPDATE parameter_tbl SET parameter_value = %s WHERE parameter_name = 'counter'"
         cursor.execute(sql, (cnt + 1))
@@ -59,11 +70,10 @@ try:
 except:
     print("error rollback")
     db.rollback()
-    db.close()
 else:
     db.commit()
+finally:
     db.close()
-
 
 # try:
 #     db = db_connect()
